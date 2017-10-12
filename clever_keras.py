37875809a2ad435d64f1eb5f70e9a1776193370c
tyxr <751021032@qq.com>
2017-10-12 20:07:12 +0800

    Update clever_keras.py

--- clever_keras.py
+++ clever_keras.py
@@ -35,7 +35,7 @@ print(encoded_Y)
 start_time = time.time()
 estimator = KerasClassifier(build_fn=create_baseline, nb_epoch=100, batch_size=5, verbose=0)
 kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
-results = cross_val_score(estimator, X, encoded_Y, cv=kfold) # 如果get_parameters方法报错,查看 https://github.com/fchollet/keras/pull/5121/commits/01c6b7180116d80845a1a6dc1f3e0fe7ef0684d8
+results = cross_val_score(estimator, X, encoded_Y, cv=kfold) 
 print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
 end_time = time.time()
 print("用时: ", end_time - start_time)
